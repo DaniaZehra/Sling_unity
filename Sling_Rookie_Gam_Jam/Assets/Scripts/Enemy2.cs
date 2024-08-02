963ac42e7133acfc498865b0396be9184fa3accb
PytHub19 <abuhurairahcs18@gmail.com>
2024-08-02 16:37:03 +0500

    Added new enemy script for level 2 + Adjusted level 2 environment variables.

---
+++ Sling_Rookie_Gam_Jam/Assets/Scripts/Enemy2.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+
+    static int totalEnemies = 3;
+    private Animator animator;
+
+    void Start()
+    {
+        Debug.Log("Remaining Enemies: " + totalEnemies);
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enemy hit by player. Destroying enemy!");
+            totalEnemies--;
+            StartCoroutine(EnemyKillSequence());
+
+            if (totalEnemies > 0)
+            {
+                Debug.Log("Remaining Enemies: " + totalEnemies);
+            }
+            else
+            {
+                Debug.Log("Enemy killed!");
+                Debug.Log("Level_2 completed");
+            }
+        }
+    }
+
+    IEnumerator EnemyKillSequence()
+    {
+        // Trigger kill animation
+        animator.SetTrigger("TriKill");
+
+        // Wait for 1 second
+        yield return new WaitForSeconds(2f);
+
+        // Destroy enemy game object
+        Destroy(gameObject);
+    }
+}
