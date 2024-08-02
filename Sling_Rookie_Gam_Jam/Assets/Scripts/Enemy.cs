4045147fa118f8de4450cd759fc44a4b1c76aa00
PytHub19 <abuhurairahcs18@gmail.com>
2024-08-03 00:32:17 +0500

    Adjusted Home Screen view.

--- Sling_Rookie_Gam_Jam/Assets/Scripts/Enemy.cs
+++ Sling_Rookie_Gam_Jam/Assets/Scripts/Enemy.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
+{  
     static int totalEnemies = 1;
     private Animator animator;
 
