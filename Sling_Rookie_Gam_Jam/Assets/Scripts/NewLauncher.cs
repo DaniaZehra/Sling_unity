01dd5daddeeb58b40d784fe8a1afc60fc15f4beb
Dania <daniazehra332@gmail.com>
2024-08-06 18:30:50 +0500

    LevelFailed Screens

--- Sling_Rookie_Gam_Jam/Assets/Scripts/NewLauncher.cs
+++ Sling_Rookie_Gam_Jam/Assets/Scripts/NewLauncher.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 
-public class AimAndShoot : MonoBehaviour
+public class NewLauncher : MonoBehaviour
 {
-    public GameObject projectilePrefab; // The object to shoot
-    public Transform shootPoint; // The point from which the projectile is shot
-    public float projectileSpeed = 10f; // The speed of the projectile
+    public GameObject projectilePrefab; 
+    public Transform shootPoint; 
+    public float projectileSpeed = 10f; 
+    public int trajectoryPoints = 30;
+    public float timeBetweenPoints = 0.1f;
+    private bool isAiming;
+    private LineRenderer lineRender;
 
+    void Start(){
+        lineRender = GetComponent<LineRenderer>();
+        lineRender.material = new Material(Shader.Find("Sprites/Default"));
+        lineRender.startWidth = 0.1f;
+        lineRender.endWidth = 0.1f;
+    }
     void Update()
     {
-        AimAtMouse();
-
-        if (Input.GetMouseButtonDown(0)) // Left mouse button
+       if (Input.GetMouseButtonDown(0)) 
         {
-            Shoot();
+            isAiming = true;
+        }
+
+        if (isAiming)
+        {
+            AimAtMouse();
+            ShowTrajectory();
+        }
+
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            if (isAiming)
+            {
+                Shoot();
+                isAiming = false;
+            }
         }
     }
 
@@ -35,5 +58,29 @@ public class AimAndShoot : MonoBehaviour
         GameObject projectile = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.velocity = shootPoint.forward * projectileSpeed;
+    }
+    void ShowTrajectory(){
+         Vector3[] points = new Vector3[trajectoryPoints];
+        Vector3 startingPosition = shootPoint.position;
+        Vector3 startingVelocity = shootPoint.forward * projectileSpeed;
+
+        for (int i = 0; i < trajectoryPoints; i++)
+        {
+            float time = i * timeBetweenPoints;
+            Vector3 point = startingPosition + startingVelocity * time;
+            point.y = startingPosition.y;
+
+            points[i] = point;
+            if (i > 0 && Physics.Linecast(points[i - 1], points[i], out RaycastHit hit))
+            {
+                points[i] = hit.point;
+                lineRender.positionCount = i + 1;
+                lineRender.SetPositions(points);
+                return;
+            }
+        }
+
+        lineRender.positionCount = trajectoryPoints;
+        lineRender.SetPositions(points);
     }
 }
