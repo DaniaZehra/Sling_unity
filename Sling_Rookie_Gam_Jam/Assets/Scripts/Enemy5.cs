db98ffda82dd14f517584c7de48b42f9eb73b903
PytHub19 <abuhurairahcs18@gmail.com>
2024-08-07 18:07:38 +0500

    Added enemy script for Level_5.
    Finally done with Mechanics.

---
+++ Sling_Rookie_Gam_Jam/Assets/Scripts/Enemy5.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy5 : MonoBehaviour
+{
+
+    static int totalEnemies = 5;
+    private Animator animator;
+
+    void Start()
+    {
+        Debug.Log("Remaining Enemies: " + totalEnemies);
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enemy hit by player. Destroying enemy!");
+            StartCoroutine(EnemyKillSequence());
+
+            if (totalEnemies > 0)
+            {
+                Debug.Log("Remaining Enemies: " + totalEnemies);
+            }
+            else
+            {
+                Debug.Log("Enemy killed!");
+                Debug.Log("Level_4 completed");
+            }
+        }
+    }
+
+    IEnumerator EnemyKillSequence()
+    {
+        // Trigger kill animation
+        animator.SetTrigger("TriKill");
+
+        // Wait for 1 second
+        yield return new WaitForSeconds(1f);
+
+        // Destroy enemy game object
+        Destroy(gameObject);
+        totalEnemies--;
+    }
+}
